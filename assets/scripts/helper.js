2e1bc319336f0a667830d766bb7835ad039ed51c
a <aWiz@localhost.localdomain>
2024-07-10 21:14:15 -0400

    Added new photos. Switched the ordering of the photos. Added the 3 cards explaining where to throw a party.

--- assets/scripts/helper.js
+++ assets/scripts/helper.js
@@ -66,4 +66,24 @@ class UtilityHelper {
     addFunctionToWindowEvent(windowEvent, callback) {
         window.addEventListener(windowEvent, callback);
     }
+
+    initPhotoSlider(className)  {
+        const photos = document.querySelectorAll(`.${className}`);
+        let currentIndex = 0;
+        const intervalTime = 6000; // Time in milliseconds (3 seconds)
+
+        function switchPhoto() {
+            // Remove 'active' class from the current photo
+            photos[currentIndex].classList.remove('active');
+
+            // Calculate the index of the next photo
+            currentIndex = (currentIndex + 1) % photos.length;
+
+            // Add 'active' class to the next photo
+            photos[currentIndex].classList.add('active');
+        }
+
+        // Set interval to switch photos
+        setInterval(switchPhoto, intervalTime);
+    }
 }
